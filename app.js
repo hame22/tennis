8ff759f274890511824b115c0fd7c9ea2c8ef394
hame22 <alex@Alexs-MacBook-Pro.local>
2014-07-12 23:22:08 +0100

    Config and initial root

---
+++ app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var express = require('express'),
+config = require('./lib/configuration'),
+routes = require('./lib/routes');
+
+var app = express();
+
+app.set('port', config.get('server:port'));
+
+app.set('port', 3000);
+
+app.get('/', routes.index);
+
+app.listen(app.get('port'), function () {
+	console.log('Listening....');
+});
