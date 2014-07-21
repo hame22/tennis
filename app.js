68b87141447d2f232e996f82d6c5edc8a3c52ef2
hame22 <alex.hamer@gmail.com>
2014-07-21 20:14:10 +0100

    Consume players api and initial search post

--- app.js
+++ app.js
@@ -2,7 +2,8 @@
 
 var express = require('express'),
 config = require('./lib/configuration'),
-routes = require('./lib/routes');
+routes = require('./lib/routes'),
+bodyParser = require('body-parser');
 
 var app = express();
 
@@ -10,8 +11,11 @@ app.set('port', config.get('server:port'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.raw());
 
 app.get('/', routes.index);
+
+app.post('/headToHead', routes.headToHead);
 
 app.listen(app.get('port'), function () {
 	console.log('Listening....');
