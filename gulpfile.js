c8d55b0d6fb5dcd11881e171e78a153e5ae86c92
hame22 <alex.hamer@gmail.com>
2014-07-27 20:41:31 +0100

    initial views but initial gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp'),
+sass = require('gulp-sass'),
+changed = require('gulp-changed'),
+ngmin = require('gulp-ngmin'),
+watch = require('gulp-watch');
+
+gulp.task('sass', function () {
+	//gulp.src('./public/scss/*.scss')
+	//.pipe(changed('./public/css'))
+	//.pipe(sass())
+	//.pipe(gulp.dest('./public/css'))
+
+
+	gulp.src('./public/scss/*.scss')
+        .pipe(watch(function(files) {
+            return files.pipe(sass())
+                .pipe(gulp.dest('./public/css'));
+        }));
+});
+
+gulp.task('default', ['sass']);
+
